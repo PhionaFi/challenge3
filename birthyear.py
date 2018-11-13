9c3d79b89b05b4c832a3821ed2230998c49e787c
Phiona Fi <fiyona65@gmail.com>
2018-11-13 17:50:23 +0300

    First Commit

---
+++ birthyear.py
@@ -0,0 +1,13 @@
+input = input("how old are you?")
+age =int(input)
+if age <18:
+    print("minor")
+else:
+    if age >18 and age <37:
+        print("youth")
+    else:
+        print("elder")
+     
+
+
+
